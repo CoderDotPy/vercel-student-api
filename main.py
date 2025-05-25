b5e0092f72aea278cc77755978612a755796611b
AYUSH TIWARI <151913473+CoderDotPy@users.noreply.github.com>
2025-05-25 21:48:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
 
-# Enable CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,12 +11,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load marks from JSON file
 with open("data.json") as f:
     student_data = json.load(f)
 
 @app.get("/api")
-def get_marks(name: list[str] = []):
+def get_marks(name: list[str] = Query([])):
     name_to_marks = {student["name"]: student["marks"] for student in student_data}
     result = [name_to_marks.get(n, None) for n in name]
     return {"marks": result}
